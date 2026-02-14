5fcb6d3287bd82b28c6cc6c59f8c486a4ffd5cea
Magrys <magdalena.rys93@gmail.com>
2026-02-14 10:10:06 +0100

    Changed cart

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My first chnge to be commited 
+        // My second change.
         [Key]
         public int CartItemId { get; set; }
 
